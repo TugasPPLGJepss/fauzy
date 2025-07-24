e655baf21aaad2d5fb0f702971439663b828e053
fauzy <vallenuji160@gmail.com>
2025-07-24 13:20:09 +0700

    index.html

---
+++ fauzyy_/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body id="ubah">
+
+<p onmouseenter="madrid()" onmouseleave="GGMU()"
+> madrid kalah</p>
+<script>
+function madrid() {
+    document.getElementById('ubah').style.backgroundColor = 'blue'
+}
+function GGMU() {
+ document.getElementById('ubah').style.
+    backgroundColor = 'red'
+}
+
+
+  //  for (let i = 0; i < 1000; i++)
+  //      if (i % 5 == 0){
+   //         document.write(i + ")      
+  //  }
+
+</script>
+
+</body>
+</html>
---
+++ fauzyy_/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<input type="text" id="username">
+<input type="password" id="password">
+<button onclick="login()">login</button>
+<script>
+    function login() {
+        let username = document.getElementById('username').value
+        let password = document.getElementById('password').value
+alert(username,password)
+if (username == 'woiii' && password == 'apaaa') {
+    alert("login berhasil")
+} else {
+    alert("gagal")
+}
+    }
+    //CONGRATULATIONS YOU HAVE JUST MEET PERSIB BANDUNG
+
+
+//SALIN CODINGAN ALT+SHIFT+PANAH
+//GANTI NAMA CTRL +F2  
+  //  const username = prompt('aku')
+//const password = prompt('kamu')
+//if(username == "" && orang2 =="kamu siapa"){
+    document.write('/')
+//}else{
+ //   document.write('')
+//}
+</script>
+
+    
+</body>
+</html>
